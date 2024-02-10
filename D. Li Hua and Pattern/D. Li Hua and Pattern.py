5ddb94d5eaae992e467582c57524554000a2a805
AyoubGhoula <ayoubghoula40@gmail.com>
2024-02-10 09:25:31 +0100

    modif sub

--- D. Li Hua and Pattern/D. Li Hua and Pattern.py
+++ D. Li Hua and Pattern/D. Li Hua and Pattern.py
@@ -1,18 +1,23 @@
-pattern=[]
 for _ in range(int(input())):
+    pattern=[]
     pattern_size,k=map(int,input().split())
     for i in range(pattern_size):
         pattern.append(list(map(int,input().split())))
     count=0
-    for i in range(pattern_size//2):
+    if pattern_size%2==0:
+        n=pattern_size//2
+    else:
+        n=(pattern_size//2)+1
+    for i in range(n):
         for j in range(pattern_size):
+            if i ==n-1 and j==n-1 and pattern_size%2!=0:
+                break
             if pattern[i][j]!=pattern[pattern_size-i-1][pattern_size-j-1]:
                 count+=1
             if count>k:
                 break
         if count>k:
             break
-    print(count)
     if count>k:
         print("no")
     elif((k-count)%2)==0:
